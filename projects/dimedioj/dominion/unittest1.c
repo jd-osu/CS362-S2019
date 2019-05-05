502d1162141ed5c64d3627c7979d13800372e388
jd-osu <dimedioj@oregonstate.edu>
2019-05-05 15:50:21 -0400

    d

--- projects/dimedioj/dominion/unittest1.c
+++ projects/dimedioj/dominion/unittest1.c
@@ -50,13 +50,13 @@ int main() {
 
     int handCount_prev, deckCount_prev, discardCount_prev;
     
-    int tr1, tr2;
+    int tr1, tr2, tr1_idx, tr2_idx;
     
     bool result;
 
 
-    
-    //TEST1 - 1st 2 cards of deck are treasure cards ****************************************
+    // ************************************************************************************
+    //TEST1
     const char test1[] = "1st 2 cards of deck are treasure cards";
 
     // clear the game state
@@ -67,9 +67,11 @@ int main() {
 
     tr1 = 4;
     tr2 = 5;
+    tr1_idx = 0;
+    tr2_idx = 0;
 
-    G.deck[0][0] = tr1;
-    G.deck[0][0] = tr2;
+    G.deck[0][tr1_idx] = tr1;
+    G.deck[0][tr2_idx] = tr2;
     
     handCount_prev = G.handCount[0];
     deckCount_prev = G.deckCount[0];
@@ -82,16 +84,12 @@ int main() {
                 ((G.hand[0][G.handCount[0]-2] == tr1) && (G.hand[0][G.handCount[0]-1] == tr2)) &&
                 (G.deckCount[0] + G.discardCount[0] - deckCount_prev - discardCount_prev == -2)
               );
-    /*
-    printf("Hand diff = %d, expected = %d\n", G.handCount[0] - handCount_prev, 2);
-    printf("Last two in hand = %d,%d, expected %d, %d\n", G.hand[0][G.handCount[0]-2], G.hand[0][G.handCount[0]-1], tr1, tr2);
-    printf("Deck/discard diff = %d, expected = %d\n", G.deckCount[0] + G.discardCount[0] - deckCount_prev - discardCount_prev, -2);
-    */
 
     _assert(result, test1);
+
+
     // ************************************************************************************
-    
-    //TEST2 - last 2 cards are treasure cards *********************************************
+    //TEST2
     const char test2[] = "last 2 cards of deck are treasure cards";
 
     // clear the game state
@@ -102,13 +100,15 @@ int main() {
 
     tr1 = 4;
     tr2 = 5;
+    tr1_idx = G.deckCount[0]-2;
+    tr2_idx = G.deckCount[0]-1;
 
     for (i=0; i<G.deckCount[0]; i++)
       G.deck[0][i] = 1;
 
 
-    G.deck[0][G.deckCount[0]-2] = tr1;
-    G.deck[0][G.deckCount[0]-1] = tr2;
+    G.deck[0][tr1_idx] = tr1;
+    G.deck[0][tr2_idx] = tr2;
     
     handCount_prev = G.handCount[0];
     deckCount_prev = G.deckCount[0];
@@ -125,8 +125,49 @@ int main() {
               );
 
     _assert(result, test2);
+
+
     // ************************************************************************************
+    //TEST3
+    const char test3[] = "1 treasure card in deck, 1 treasure card in discard";
 
+    // clear the game state
+    memset(&G, 23, sizeof(struct gameState));
 
+    // initialize new game
+    initializeGame(numPlayer, k, seed, &G);
+
+    G.discardCount = 5;
+
+    tr1 = 4;
+    tr2 = 5;
+    tr1_idx = G.deckCount[0]/2;
+    tr2_idx = G.discardCount[0]/2;
+
+    for (i=0; i<G.deckCount[0]; i++)
+      G.deck[0][i] = 1;
+
+    for (i=0; i<G.discardCount[0]; i++)
+      G.discard[0][i] = 1;
+
+    G.deck[0][tr1_idx] = tr1;
+    G.discard[0][tr2_idx] = tr2;
+    
+    handCount_prev = G.handCount[0];
+    deckCount_prev = G.deckCount[0];
+    discardCount_prev = G.discardCount[0];
+    
+    // CALL FUNCTION
+    _adventurer(0, &G);
+    
+    result =  (  (G.handCount[0] - handCount_prev == 2) &&
+                ((G.hand[0][G.handCount[0]-2] == tr1) && (G.hand[0][G.handCount[0]-1] == tr2)) &&
+                (G.deckCount[0] + G.discardCount[0] - deckCount_prev - discardCount_prev == -2) &&
+              );
+
+    _assert(result, test3);
+    // ************************************************************************************
+    
+    
     return 0;
 }
