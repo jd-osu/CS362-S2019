8283b2ae8a73ea7aa228b0e77af62c8c7f46a6a2
jd-osu <dimedioj@oregonstate.edu>
2019-05-05 16:20:08 -0400

    d

--- projects/dimedioj/dominion/cardtest1.c
+++ projects/dimedioj/dominion/cardtest1.c
@@ -18,7 +18,7 @@ typedef enum {false, true} bool;
 
 const char PASS[] = "PASS";
 const char FAIL[] = "FAIL";
-const char FUNCTION[] = "_adventurer()";
+const char FUNCTION[] = "_cardEffect(ADVENTURER)";
 
 int _assert(bool condition, const char *test_name)
 {
@@ -41,6 +41,8 @@ int main() {
     int k[10] = {adventurer, council_room, feast, gardens, mine
                , remodel, smithy, village, baron, great_hall};
     struct gameState G;
+
+    int *bonus;
 
     int handCount_prev, deckCount_prev, discardCount_prev;
     
@@ -73,8 +75,7 @@ int main() {
     deckCount_prev = G.deckCount[0];
     discardCount_prev = G.discardCount[0];
     
-    // CALL FUNCTION
-    return_val = _adventurer(0, &G);
+    return_val = cardEffect(7, 0, 0, 0, &G, 0, &bonus);
     
     result =  ( (return_val == 0) &&
                 (G.handCount[0] - handCount_prev == 2) &&
@@ -87,7 +88,7 @@ int main() {
 
     // ************************************************************************************
     //TEST2
-    const char test2[] = "last 2 cards of deck are treasure cards";
+    const char test2[] = "no cards in deck";
 
     // clear the game state
     memset(&G, 23, sizeof(struct gameState));
@@ -95,52 +96,13 @@ int main() {
     // initialize new game
     initializeGame(numPlayer, k, seed, &G);
 
-    tr1 = 4;
-    tr2 = 5;
-    tr1_idx = G.deckCount[0]-2;
-    tr2_idx = G.deckCount[0]-1;
-
-    for (i=0; i<G.deckCount[0]; i++)
-      G.deck[0][i] = 1;
-
-
-    G.deck[0][tr1_idx] = tr1;
-    G.deck[0][tr2_idx] = tr2;
-    
-    handCount_prev = G.handCount[0];
-    deckCount_prev = G.deckCount[0];
-    discardCount_prev = G.discardCount[0];
-    
-    // CALL FUNCTION
-    return_val = _adventurer(0, &G);
-    
-    result =  ( (return_val == 0) &&
-                (G.handCount[0] - handCount_prev == 2) &&
-                ((G.hand[0][G.handCount[0]-2] == tr1) && (G.hand[0][G.handCount[0]-1] == tr2)) &&
-                (G.deckCount[0] + G.discardCount[0] - deckCount_prev - discardCount_prev == -2) &&
-                (G.discardCount[0] == deckCount_prev - 2) &&
-                (G.deckCount[0] == 0)
-              );
-
-    _assert(result, test2);
-
-
-    // ************************************************************************************
-    //TEST3
-    const char test3[] = "1 treasure card in deck, 1 treasure card in discard";
-
-    // clear the game state
-    memset(&G, 23, sizeof(struct gameState));
-
-    // initialize new game
-    initializeGame(numPlayer, k, seed, &G);
-
-    G.discardCount[0] = 5;
+    G.discardCount[0] = 10;
+    G.deckCount[0] = 0;
 
     tr1 = 4;
     tr2 = 5;
-    tr1_idx = G.deckCount[0]/2;
-    tr2_idx = G.discardCount[0]/2;
+    tr1_idx = 0;
+    tr2_idx = 1;
 
     for (i=0; i<G.deckCount[0]; i++)
       G.deck[0][i] = 1;
@@ -148,24 +110,81 @@ int main() {
     for (i=0; i<G.discardCount[0]; i++)
       G.discard[0][i] = 1;
 
-    G.deck[0][tr1_idx] = tr1;
+    G.discard[0][tr1_idx] = tr1;
     G.discard[0][tr2_idx] = tr2;
     
     handCount_prev = G.handCount[0];
     deckCount_prev = G.deckCount[0];
     discardCount_prev = G.discardCount[0];
     
-    // CALL FUNCTION
-    return_val = _adventurer(0, &G);
+    return_val = cardEffect(7, 0, 0, 0, &G, 0, &bonus);
     
-    result =  ( (return_val == 0) && 
+    result =  ( (return_val == 0) &&
                 (G.handCount[0] - handCount_prev == 2) &&
                 ((G.hand[0][G.handCount[0]-2] == tr1) && (G.hand[0][G.handCount[0]-1] == tr2)) &&
                 (G.deckCount[0] + G.discardCount[0] - deckCount_prev - discardCount_prev == -2)
               );
 
+    _assert(result, test2);
+
+
+    // ************************************************************************************
+    //TEST3
+    const char test3[] = "only 1 treasure card";
+
+    // clear the game state
+    memset(&G, 23, sizeof(struct gameState));
+
+    // initialize new game
+    initializeGame(numPlayer, k, seed, &G);
+
+    tr1 = 4;
+    tr1_idx = 0;
+
+    for (i=0; i<G.deckCount[0]; i++)
+      G.deck[0][i] = 1;
+
+    G.deck[0][tr1_idx] = tr1;
+    
+    handCount_prev = G.handCount[0];
+    deckCount_prev = G.deckCount[0];
+    discardCount_prev = G.discardCount[0];
+    
+    return_val = cardEffect(7, 0, 0, 0, &G, 0, &bonus);
+    
+    result =  ( (return_val == 0) &&
+                (G.handCount[0] - handCount_prev == 1) &&
+                (G.hand[0][G.handCount[0]-1] == tr1) &&
+                (G.deckCount[0] + G.discardCount[0] - deckCount_prev - discardCount_prev == -1)
+              );
+
     _assert(result, test3);
     
+    // ************************************************************************************
+    //TEST4
+    const char test4[] = "no treasure cards";
+
+    // clear the game state
+    memset(&G, 23, sizeof(struct gameState));
+
+    // initialize new game
+    initializeGame(numPlayer, k, seed, &G);
+
+    for (i=0; i<G.deckCount[0]; i++)
+      G.deck[0][i] = 1;
+    
+    handCount_prev = G.handCount[0];
+    deckCount_prev = G.deckCount[0];
+    discardCount_prev = G.discardCount[0];
+    
+    return_val = cardEffect(7, 0, 0, 0, &G, 0, &bonus);
+    
+    result =  ( (return_val == 0) &&
+                (G.handCount[0] - handCount_prev == 0) &&
+                (G.deckCount[0] + G.discardCount[0] - deckCount_prev - discardCount_prev == 0)
+              );
+
+    _assert(result, test4);
     
     return 0;
 }
