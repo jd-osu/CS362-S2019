5374913abf882d267649dd545372c22a4202ed1a
jd-osu <dimedioj@oregonstate.edu>
2019-05-05 18:21:04 -0400

    d

--- projects/dimedioj/dominion/dominion.c
+++ projects/dimedioj/dominion/dominion.c
@@ -77,8 +77,9 @@ int _mine(int player, struct gameState *state, int pos, int c1, int c2) {
   gainCard(c2, state, 2, player);
 
   //discard card from hand
-  discardCard(pos, player, state, 0);
+  discardCard(pos, player, state, 1);
 
+/*
   //discard trashed card
   for (i = 0; i < state->handCount[player]; i++)
 	{
@@ -88,6 +89,7 @@ int _mine(int player, struct gameState *state, int pos, int c1, int c2) {
 	    break;
 	  }
 	}
+*/
   
   return 0;
 }
