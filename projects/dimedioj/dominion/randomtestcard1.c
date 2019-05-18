943f434419139cf06ff0c030ffbb63af72dbcea8
jd-osu <dimedioj@oregonstate.edu>
2019-05-18 18:18:43 -0400

    d

--- projects/dimedioj/dominion/randomtestcard1.c
+++ projects/dimedioj/dominion/randomtestcard1.c
@@ -104,8 +104,10 @@ void test_run()
   int selected_card_idx, selected_card, selected_card_cost;
   int num_total, num_total_prev, num_deck, num_deck_prev, num_hand, num_hand_prev, num_discard, num_discard_prev;
   int coins_prev, coins, eval_coins;
+  int salvager_pos;
+  int num_played, num_played_prev;
   int buys_prev, buys, eval_buys;
-  int eval_trash;
+  int eval_trash, eval_played;
     
   int return_val;
     
@@ -125,23 +127,32 @@ void test_run()
   num_total_prev = get_random_number(0,500);
   num_deck_prev = get_random_number(0, num_total_prev);
   num_discard_prev = get_random_number(0, num_total_prev - num_deck_prev);
-  num_hand_prev = num_total_prev - num_deck_prev - num_discard_prev;
+  num_hand_prev = get_random_number(0, num_total_prev - num_deck_prev - num_discard_prev);
+  num_played_prev = num_total_prev - num_deck_prev - num_discard_prev - num_hand_prev;
   selected_card_idx = get_random_number(0, num_hand_prev - 1);
   coins_prev = get_random_number(0, 100);
   buys_prev = get_random_number(0, 100);
-
+  
+  if (num_hand_prev == 0)
+    salvager_pos = 0;
+  else
+    salvager_pos = get_random_number(0,num_hand_prev - 1);
+   
   printf("num_total_prev=%d\n", num_total_prev);
   printf("num_deck_prev=%d\n", num_deck_prev);
   printf("num_hand_prev=%d\n", num_hand_prev);
   printf("num_discard_prev=%d\n", num_discard_prev);
+  printf("num_played_prev=%d\n", num_played_prev);
   printf("selected_card_idx=%d\n", selected_card_idx);
   printf("coins_prev=%d\n", coins_prev);
   printf("buys_prev=%d\n", buys_prev);
+  printf("salvager_pos=%d\n", salvager_pos);
   
   // modify new game based on random parameters  
   G.deckCount[0] = num_deck_prev;
   G.handCount[0] = num_hand_prev;
   G.discardCount[0] = num_discard_prev;
+  G.playedCardCount = num_played_prev;
 
   // populate deck, hand and discard with random cards	
   for (i = 0; i < G.deckCount[0]; i++)
@@ -152,23 +163,31 @@ void test_run()
 	  
   for (i = 0; i < G.discardCount[0]; i++)
     G.discard[0][i] = get_random_card();
+  
+  for (i = 0; i < G.playedCardCount; i++)
+    G.playedCards[i] = get_random_card();
 
+  if (num_hand_prev > 0)
+    G.hand[0][salvager_pos] = salvager;    
+  
   G.coins = coins_prev;
   G.numBuys = buys_prev;
 
   if (num_hand_prev > 0)
   {
-	selected_card = G.hand[0][selected_card_idx];
+	  selected_card = G.hand[0][selected_card_idx];
     selected_card_cost = getCost(selected_card);
-	eval_coins = selected_card_cost;
-	eval_trash = -1;
+	  eval_coins = selected_card_cost;
+	  eval_trash = -1;
+    eval_played = 1;
   }
   else
   {
-	selected_card = -1;
-	selected_card_cost = -99999;
-	eval_coins = 0;
-	eval_trash = 0;
+    selected_card = -1;
+    selected_card_cost = -99999;
+    eval_coins = 0;
+    eval_trash = 0;
+    eval_played = -99999;
   }
 
   eval_buys = buys_prev + 1;
@@ -176,25 +195,28 @@ void test_run()
   printf("selected_card_cost=%d\n", selected_card_cost);  
   printf("eval_coins=%d\n", eval_coins);
   printf("eval_buys=%d\n", eval_buys);
-  printf("eval_trash=%d\n", eval_trash);   
+  printf("eval_trash=%d\n", eval_trash);
+  printf("eval_played=%d\n", eval_played);
 
 
 
   //display_state(&G);
 
 
-  return_val = _salvager(0, &G, );
-  printf("return_val=%d\n", return_val);
+  //return_val = _salvager(0, &G, );
+  //printf("return_val=%d\n", return_val);
 
   // get resulting data
   num_deck = G.deckCount[0];
   num_hand = G.handCount[0];
   num_discard = G.discardCount[0];
-  num_total = num_deck + num_hand + num_discard;
+  num_played = G.playedCardCount;
+  num_total = num_deck + num_hand + num_discard + num_played;
   printf("num_total=%d\n", num_total);
   printf("num_deck=%d\n", num_deck);
   printf("num_hand=%d\n", num_hand);
   printf("num_discard=%d\n", num_discard);
+  printf("num_played=%d\n", num_played);
   
   coins = G.coins;
   buys = G.numBuys;
