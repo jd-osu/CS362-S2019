a8b4d32cec918d7c14f7700647ca5967a6cbb676
jd-osu <dimedioj@oregonstate.edu>
2019-05-18 19:16:57 -0400

    Revert "d"
    This reverts commit 92f4eb14c6d3f12ddc1f64f3085552f09b64b6b2.

--- projects/dimedioj/dominion/dominion.c
+++ projects/dimedioj/dominion/dominion.c
@@ -5,6 +5,40 @@
 #include <math.h>
 #include <stdlib.h>
 
+int _adventurer(int player, struct gameState *state) {
+  int drawntreasure=0;
+  int cardDrawn;
+  int temphand[MAX_HAND];
+  int z = 0;// this is the counter for the temp hand
+
+
+  while(drawntreasure<2){
+    if (state->deckCount[player] < 1){//if the deck is empty we need to shuffle discard and add to deck
+      shuffle(player, state);
+    }
+    
+    drawCard(player, state);
+	
+    cardDrawn = state->hand[player][state->handCount[player]-1];//top card of hand is most recently drawn card.
+    
+    if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
+      drawntreasure++;
+    else{
+      temphand[z]=cardDrawn;
+      state->handCount[player]--; //this should just remove the top card (the most recently drawn one).
+      z++;
+    }
+  }
+
+  while(z-1>=0){
+    state->discard[player][state->discardCount[player]++]=temphand[z-1]; // discard all cards in play that have been drawn
+    
+    z=z-1;
+  }
+  
+  return 0;
+}
+
 int _smithy(int player, struct gameState *state, int pos) {
   int i;
   
@@ -56,6 +90,26 @@ int _mine(int player, struct gameState *state, int pos, int c1, int c2) {
 	  }
 	}
 
+  
+  return 0;
+}
+
+int _salvager(int player, struct gameState *state, int pos, int c1) {
+
+  //+1 buy
+  state->numBuys++;
+			
+  if (c1)
+	{
+	  //gain coins equal to trashed card
+	  state->coins = state->coins + getCost( handCard(c1, state) );
+	  
+    //trash card
+	  discardCard(c1, player, state, 1);	
+	}
+			
+  //discard card
+  discardCard(pos, player, state, 0);
   
   return 0;
 }
