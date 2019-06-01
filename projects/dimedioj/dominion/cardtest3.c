f8da715a82464c0379f62f0015a74c41083b0430
jd-osu <dimedioj@oregonstate.edu>
2019-06-01 10:21:53 -0400

    Added my test functions and Makefile to teammate's code.

---
+++ projects/dimedioj/dominion/cardtest3.c
@@ -0,0 +1,264 @@
+/* -----------------------------------------------------------------------
+ * Author: Jason DiMedio
+ * CS362, Spring 2019
+ * NOTE: The following code was adapted from testUpdateCoins.c provided as a
+ * template to be used in Assignment 3 for CS362.
+ * -----------------------------------------------------------------------
+ */
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+
+// bool type defined
+typedef enum {false, true} bool;
+
+const char PASS[] = "PASS";
+const char FAIL[] = "FAIL";
+const char FUNCTION[] = "cardEffect(MINE)";
+
+void display_state(struct gameState *state)
+{
+  int i;
+  
+  printf("HAND: %d\n", state->handCount[0]);
+  for (i=0; i<state->handCount[0]; i++)
+    printf("%d : %d\n", i, state->hand[0][i]);
+  printf("\n");
+  
+  printf("DECK: %d\n", state->deckCount[0]);
+  for (i=0; i<state->deckCount[0]; i++)
+    printf("%d : %d\n", i, state->deck[0][i]);
+  printf("\n");
+  
+  printf("DISCARD: %d\n", state->discardCount[0]);
+  for (i=0; i<state->discardCount[0]; i++)
+    printf("%d : %d\n", i, state->discard[0][i]);
+  printf("\n");
+  
+  printf("PLAYED: %d\n", state->playedCardCount);
+  for (i=0; i<state->playedCardCount; i++)
+    printf("%d : %d\n", i, state->playedCards[i]);
+  printf("\n");
+  
+  printf("SUPPLY:\n");
+  for (i=0; i<=treasure_map; i++)
+    printf("%d : %d\n", i, state->supplyCount[i]);
+  printf("\n");
+  
+}
+
+
+
+int _assert(bool condition, const char *test_name)
+{
+  printf("%s: ", FUNCTION);
+  
+  if (condition == true)
+    printf("%s", PASS);
+  else
+    printf("%s", FAIL);
+  
+  printf(" when %s\n", test_name);
+}
+
+int main() {
+    int i;
+    int seed = 1000;
+    int numPlayer = 2;
+    int p, r, handCount;
+    int k[10] = {adventurer, council_room, feast, gardens, mine
+               , remodel, smithy, village, baron, great_hall};
+    struct gameState G;
+
+    int handCount_prev, deckCount_prev, discardCount_prev, playedCount_prev, c2_qty_Prev;
+    
+    int c1, c2, c1_idx, c2_qty, mine_idx;
+    
+    int *bonus;
+    
+    int return_val;
+    
+    bool result, c2_result;
+
+
+    // ************************************************************************************
+    //TEST1
+    const char test1[] = "c1= copper, c2= silver, hand >1, c2_qty > 0";
+
+    // clear the game state
+    memset(&G, 23, sizeof(struct gameState));
+
+    // initialize new game
+    initializeGame(numPlayer, k, seed, &G);
+
+    c1 = copper;
+    c2 = silver;
+    c1_idx = G.handCount[0]/2;
+    mine_idx = 0;
+    //c2_qty = 
+
+    G.hand[0][c1_idx] = c1;
+    G.hand[0][mine_idx] = mine;
+    
+    handCount_prev = G.handCount[0];
+    deckCount_prev = G.deckCount[0];
+    discardCount_prev = G.discardCount[0];
+    playedCount_prev = G.playedCardCount;
+    c2_qty_Prev = G.supplyCount[c2];
+    
+    //display_state(&G);    
+    
+    return_val = cardEffect(mine, c1_idx, c2, 0, &G, mine_idx, bonus);
+
+    //display_state(&G);    
+    
+    c2_result = false;
+    for (i=0; i<G.handCount[0]; i++)
+    {
+      if (G.hand[0][i] == c2)
+      {
+        c2_result = true;
+        break;
+      }
+    }
+    
+    result =  ( (return_val == 0) &&
+                (G.handCount[0] - handCount_prev == -1) &&
+                (G.playedCardCount - playedCount_prev == 1) &&
+                (G.discardCount[0] - discardCount_prev == 0) &&
+                (c2_result) &&
+                (G.supplyCount[c2] - c2_qty_Prev == -1)
+              );
+
+    _assert(result, test1);
+    
+    
+    // ************************************************************************************
+    //TEST2
+    const char test2[] = "c1= adventurer (out of range)";
+
+    // clear the game state
+    memset(&G, 23, sizeof(struct gameState));
+
+    // initialize new game
+    initializeGame(numPlayer, k, seed, &G);
+
+    c1 = adventurer;
+    c2 = silver;
+    c1_idx = G.handCount[0]/2;
+    mine_idx = 0;
+    //c2_qty = 
+
+    G.hand[0][c1_idx] = c1;
+    G.hand[0][mine_idx] = mine;
+    
+    handCount_prev = G.handCount[0];
+    deckCount_prev = G.deckCount[0];
+    discardCount_prev = G.discardCount[0];
+    playedCount_prev = G.playedCardCount;
+    c2_qty_Prev = G.supplyCount[c2];
+    
+    //display_state(&G);    
+    
+    return_val = cardEffect(mine, c1_idx, c2, 0, &G, mine_idx, bonus);
+
+    //display_state(&G);    
+    
+    c2_result = false;
+    for (i=0; i<G.handCount[0]; i++)
+    {
+      if (G.hand[0][i] == c2)
+      {
+        c2_result = true;
+        break;
+      }
+    }
+    
+    result =  ( (return_val == -1)
+              );
+
+    _assert(result, test2);
+
+    // ************************************************************************************
+    //TEST3
+    const char test3[] = "c1= copper, c2= gold (cost too high)";
+
+    // clear the game state
+    memset(&G, 23, sizeof(struct gameState));
+
+    // initialize new game
+    initializeGame(numPlayer, k, seed, &G);
+
+    c1 = copper;
+    c2 = gold;
+    c1_idx = G.handCount[0]/2;
+    mine_idx = 0;
+    //c2_qty = 
+
+    G.hand[0][c1_idx] = c1;
+    G.hand[0][mine_idx] = mine;
+    
+    handCount_prev = G.handCount[0];
+    deckCount_prev = G.deckCount[0];
+    discardCount_prev = G.discardCount[0];
+    playedCount_prev = G.playedCardCount;
+    c2_qty_Prev = G.supplyCount[c2];
+    
+    //display_state(&G);    
+    
+    return_val = cardEffect(mine, c1_idx, c2, 0, &G, mine_idx, bonus);
+
+    //display_state(&G);   
+    
+    result =  ( (return_val == -1)
+              );
+
+    _assert(result, test3);
+    
+
+    // ************************************************************************************
+    //TEST4
+    const char test4[] = "c1= copper, c2= 9999 (out of range)";
+
+    // clear the game state
+    memset(&G, 23, sizeof(struct gameState));
+
+    // initialize new game
+    initializeGame(numPlayer, k, seed, &G);
+
+    c1 = copper;
+    c2 = 9999;
+    c1_idx = G.handCount[0]/2;
+    mine_idx = 0;
+    //c2_qty = 
+
+    G.hand[0][c1_idx] = c1;
+    G.hand[0][mine_idx] = mine;
+    
+    handCount_prev = G.handCount[0];
+    deckCount_prev = G.deckCount[0];
+    discardCount_prev = G.discardCount[0];
+    playedCount_prev = G.playedCardCount;
+    //c2_qty_Prev = G.supplyCount[c2];
+    
+    //display_state(&G);    
+    
+    return_val = cardEffect(mine, c1_idx, c2, 0, &G, mine_idx, bonus);
+
+    //display_state(&G);     
+
+    result =  ( (return_val == -1)
+              );
+
+    _assert(result, test4);
+
+
+
+
+    
+    return 0;
+}
---
+++ projects/dimedioj/dominion/unittest4.c
@@ -0,0 +1,401 @@
+/* -----------------------------------------------------------------------
+ * Author: Jason DiMedio
+ * CS362, Spring 2019
+ * NOTE: The following code was adapted from testUpdateCoins.c provided as a
+ * template to be used in Assignment 3 for CS362.
+ * -----------------------------------------------------------------------
+ */
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+
+// bool type defined
+typedef enum {false, true} bool;
+
+const char PASS[] = "PASS";
+const char FAIL[] = "FAIL";
+const char FUNCTION[] = "_sea_hag()";
+
+void display_state(struct gameState *state)
+{
+  int i, p;
+
+  for (p = 0; p<state->numPlayers; p++)
+  {
+    printf("player: %d\n", p);
+    
+    printf("HAND: %d\n", state->handCount[p]);
+    for (i=0; i<state->handCount[p]; i++)
+      printf("%d : %d\n", i, state->hand[p][i]);
+    printf("DECK: %d\n", state->deckCount[p]);
+    for (i=0; i<state->deckCount[p]; i++)
+      printf("%d : %d\n", i, state->deck[p][i]);
+    printf("DISCARD: %d\n", state->discardCount[p]);
+    for (i=0; i<state->discardCount[p]; i++)
+      printf("%d : %d\n", i, state->discard[p][i]);
+    printf("\n"); 
+  }
+  
+  printf("PLAYED: %d\n", state->playedCardCount);
+  for (i=0; i<state->playedCardCount; i++)
+    printf("%d : %d\n", i, state->playedCards[i]);
+  printf("\n");
+  
+  printf("SUPPLY:\n");
+  for (i=0; i<=0; i++)
+    printf("%d : %d\n", i, state->supplyCount[i]);
+  printf("\n");
+}
+
+
+
+int _assert(bool condition, const char *test_name)
+{
+  printf("%s: ", FUNCTION);
+  
+  if (condition == true)
+    printf("%s", PASS);
+  else
+    printf("%s", FAIL);
+  
+  printf(" when %s\n", test_name);
+}
+
+int main() {
+    int i;
+    int seed = 1000;
+    int numPlayer;
+    int p, r, handCount;
+    int k[10] = {adventurer, council_room, feast, gardens, mine
+               , remodel, smithy, village, baron, great_hall};
+    struct gameState G;
+
+    int handCount_prev[10], deckCount_prev[10], discardCount_prev[10], playedCount_prev, curse_qty_Prev;
+    
+    int p2_deck, p2_discard, hag_idx, curse_qty;
+    
+    int return_val;
+    
+    bool result;
+
+
+    // ************************************************************************************
+    //TEST1
+    const char test1[] = "players=2, curse supply >0, deck>0, p1's top card != curse";
+
+    numPlayer = 2;
+
+    // clear the game state
+    memset(&G, 23, sizeof(struct gameState));
+
+    // initialize new game
+    initializeGame(numPlayer, k, seed, &G);
+
+    hag_idx = 0;
+    //curse_qty =
+    p2_deck = 5;
+    p2_discard = 5;
+    G.hand[0][hag_idx] = sea_hag;
+
+    for (p=1; p<G.numPlayers; p++)
+    { 
+      G.deckCount[p] = p2_deck;
+      G.discardCount[p] = p2_discard;
+    }
+
+    for (p=0; p<G.numPlayers; p++)
+    { 
+      handCount_prev[p] = G.handCount[p];
+      deckCount_prev[p] = G.deckCount[p];
+      discardCount_prev[p] = G.discardCount[p];
+    }
+    playedCount_prev = G.playedCardCount;
+    curse_qty_Prev = G.supplyCount[curse];
+
+    //display_state(&G);    
+    
+    return_val = _sea_hag(0, &G);
+    
+    //display_state(&G);    
+    
+    result =  ( (return_val == 0) &&
+                (G.handCount[0] - handCount_prev[0] == -1) &&
+                (G.deck[0][G.deckCount[0]-1] != curse) &&
+                (G.deck[1][G.deckCount[0]-1] == curse) &&
+                (G.deckCount[1] - deckCount_prev[1] == 0) &&
+                (G.playedCardCount - playedCount_prev == 1) &&
+                (G.discardCount[1] - discardCount_prev[1] == 1) &&
+                (G.supplyCount[curse] - curse_qty_Prev == -1)
+              );
+
+    _assert(result, test1);
+    
+    
+    // ************************************************************************************
+    //TEST2
+    const char test2[] = "players=4, curse supply >0, deck>0, p1's top card != curse";
+
+    numPlayer = 4;
+
+    // clear the game state
+    memset(&G, 23, sizeof(struct gameState));
+
+    // initialize new game
+    initializeGame(numPlayer, k, seed, &G);
+
+    hag_idx = 0;
+    //curse_qty =
+    p2_deck = 5;
+    p2_discard = 5;
+    G.hand[0][hag_idx] = sea_hag;
+
+    for (p=1; p<G.numPlayers; p++)
+    { 
+      G.deckCount[p] = p2_deck;
+      G.discardCount[p] = p2_discard;
+    }
+
+    for (p=0; p<G.numPlayers; p++)
+    { 
+      handCount_prev[p] = G.handCount[p];
+      deckCount_prev[p] = G.deckCount[p];
+      discardCount_prev[p] = G.discardCount[p];
+    }
+    playedCount_prev = G.playedCardCount;
+    curse_qty_Prev = G.supplyCount[curse];
+
+    //display_state(&G);    
+    
+    return_val = _sea_hag(0, &G);
+    
+    //display_state(&G);    
+    
+    result =  ( (return_val == 0) &&
+                (G.handCount[0] - handCount_prev[0] == -1) &&
+                (G.deck[0][G.deckCount[0]-1] != curse) &&
+                (G.deck[1][G.deckCount[0]-1] == curse) &&
+                (G.deckCount[1] - deckCount_prev[1] == 0) &&
+                (G.discardCount[1] - discardCount_prev[1] == 1) &&
+                (G.deck[2][G.deckCount[0]-1] == curse) &&
+                (G.deckCount[2] - deckCount_prev[2] == 0) &&
+                (G.discardCount[2] - discardCount_prev[2] == 1) &&                
+                (G.deck[3][G.deckCount[0]-1] == curse) &&
+                (G.deckCount[3] - deckCount_prev[3] == 0) &&
+                (G.discardCount[3] - discardCount_prev[3] == 1) &&
+                (G.playedCardCount - playedCount_prev == 1) &&
+                (G.supplyCount[curse] - curse_qty_Prev == -3)
+              );
+
+    _assert(result, test2);
+
+
+    // ************************************************************************************
+    //TEST3
+    const char test3[] = "players=2, p2 deck is empty";
+
+    numPlayer = 2;
+
+    // clear the game state
+    memset(&G, 23, sizeof(struct gameState));
+
+    // initialize new game
+    initializeGame(numPlayer, k, seed, &G);
+
+    hag_idx = 0;
+    //curse_qty =
+    p2_deck = 0;
+    p2_discard = 5;
+    G.hand[0][hag_idx] = sea_hag;
+
+    for (p=1; p<G.numPlayers; p++)
+    { 
+      G.deckCount[p] = p2_deck;
+      G.discardCount[p] = p2_discard;
+    }
+
+    for (p=0; p<G.numPlayers; p++)
+    { 
+      handCount_prev[p] = G.handCount[p];
+      deckCount_prev[p] = G.deckCount[p];
+      discardCount_prev[p] = G.discardCount[p];
+    }
+    playedCount_prev = G.playedCardCount;
+    curse_qty_Prev = G.supplyCount[curse];
+
+    //display_state(&G);    
+    
+    return_val = _sea_hag(0, &G);
+    
+    //display_state(&G);    
+    
+    result =  ( (return_val == 0) &&
+                (G.handCount[0] - handCount_prev[0] == -1) &&
+                (G.deck[0][G.deckCount[0]-1] != curse) &&
+                (G.deck[1][G.deckCount[0]-1] == curse) &&
+                (G.deckCount[1] - deckCount_prev[1] == 0) &&
+                (G.playedCardCount - playedCount_prev == 1) &&
+                (G.discardCount[1] - discardCount_prev[1] == 1) &&
+                (G.supplyCount[curse] - curse_qty_Prev == -1)
+              );
+
+    _assert(result, test3);
+
+
+    // ************************************************************************************
+    //TEST4
+    const char test4[] = "players=2, p2 deck is empty, p2 discard is empty";
+
+    numPlayer = 2;
+
+    // clear the game state
+    memset(&G, 23, sizeof(struct gameState));
+
+    // initialize new game
+    initializeGame(numPlayer, k, seed, &G);
+
+    hag_idx = 0;
+    //curse_qty =
+    p2_deck = 0;
+    p2_discard = 0;
+    G.hand[0][hag_idx] = sea_hag;
+
+    for (p=1; p<G.numPlayers; p++)
+    { 
+      G.deckCount[p] = p2_deck;
+      G.discardCount[p] = p2_discard;
+    }
+
+    for (p=0; p<G.numPlayers; p++)
+    { 
+      handCount_prev[p] = G.handCount[p];
+      deckCount_prev[p] = G.deckCount[p];
+      discardCount_prev[p] = G.discardCount[p];
+    }
+    playedCount_prev = G.playedCardCount;
+    curse_qty_Prev = G.supplyCount[curse];
+
+    //display_state(&G);    
+    
+    return_val = _sea_hag(0, &G);
+    
+    //display_state(&G);    
+    
+    result =  ( (return_val != 0)
+              );
+
+    _assert(result, test4);
+
+
+    // ************************************************************************************
+    //TEST5
+    const char test5[] = "players=2, curse supply = 0";
+
+    numPlayer = 2;
+
+    // clear the game state
+    memset(&G, 23, sizeof(struct gameState));
+
+    // initialize new game
+    initializeGame(numPlayer, k, seed, &G);
+
+    hag_idx = 0;
+    curse_qty = 0;
+    p2_deck = 5;
+    p2_discard = 5;
+    G.hand[0][hag_idx] = sea_hag;
+    G.supplyCount[curse] = curse_qty;
+
+    for (p=1; p<G.numPlayers; p++)
+    { 
+      G.deckCount[p] = p2_deck;
+      G.discardCount[p] = p2_discard;
+    }
+
+    for (p=0; p<G.numPlayers; p++)
+    { 
+      handCount_prev[p] = G.handCount[p];
+      deckCount_prev[p] = G.deckCount[p];
+      discardCount_prev[p] = G.discardCount[p];
+    }
+    playedCount_prev = G.playedCardCount;
+    curse_qty_Prev = G.supplyCount[curse];
+
+    //display_state(&G);    
+    
+    return_val = _sea_hag(0, &G);
+    
+    //display_state(&G);    
+    
+    result =  ( (return_val == 0) &&
+                (G.handCount[0] - handCount_prev[0] == -1) &&
+                (G.deck[0][G.deckCount[0]-1] != curse) &&
+                (G.deckCount[1] - deckCount_prev[1] == 0) &&
+                (G.discardCount[1] - discardCount_prev[1] == 1) &&
+                (G.playedCardCount - playedCount_prev == 1) &&
+                (G.supplyCount[curse] == 0)
+              );
+
+    _assert(result, test5);
+
+
+    // ************************************************************************************
+    //TEST6
+    const char test6[] = "players=4, curse supply < number of players";
+
+    numPlayer = 4;
+
+    // clear the game state
+    memset(&G, 23, sizeof(struct gameState));
+
+    // initialize new game
+    initializeGame(numPlayer, k, seed, &G);
+
+    hag_idx = 0;
+    curse_qty = 1;
+    p2_deck = 5;
+    p2_discard = 5;
+    G.hand[0][hag_idx] = sea_hag;
+
+    for (p=1; p<G.numPlayers; p++)
+    { 
+      G.deckCount[p] = p2_deck;
+      G.discardCount[p] = p2_discard;
+    }
+
+    for (p=0; p<G.numPlayers; p++)
+    { 
+      handCount_prev[p] = G.handCount[p];
+      deckCount_prev[p] = G.deckCount[p];
+      discardCount_prev[p] = G.discardCount[p];
+    }
+    playedCount_prev = G.playedCardCount;
+    curse_qty_Prev = G.supplyCount[curse];
+
+    //display_state(&G);    
+    
+    return_val = _sea_hag(0, &G);
+    
+    //display_state(&G);    
+    
+    result =  ( (return_val == 0) &&
+                (G.handCount[0] - handCount_prev[0] == -1) &&
+                (G.deck[0][G.deckCount[0]-1] != curse) &&
+                (G.deck[1][G.deckCount[0]-1] == curse) &&
+                (G.deckCount[1] - deckCount_prev[1] == 0) &&
+                (G.discardCount[1] - discardCount_prev[1] == 1) &&
+                (G.deckCount[2] - deckCount_prev[2] == 0) &&
+                (G.discardCount[2] - discardCount_prev[2] == 1) &&                
+                (G.deckCount[3] - deckCount_prev[3] == 0) &&
+                (G.discardCount[3] - discardCount_prev[3] == 1) &&
+                (G.playedCardCount - playedCount_prev == 1) &&
+                (G.supplyCount[curse] == 0)
+              );
+
+    _assert(result, test6);
+
+    return 0;
+}
